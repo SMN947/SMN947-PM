e3e0e81e48040f851f256b17b074dc379c45df62
SMN947 <betum98@gmail.com>
2021-09-09 23:55:30 -0500

    Added localhost db vars

--- config/config.php
+++ config/config.php
@@ -1,2 +1,7 @@
 <?php
     const BASE_URL = "http://localhost/SMN947-PM/";
+    const DB_HOST = "localhost";
+    const DB_NAME = "smn947-pm";
+    const DB_USER = "root";
+    const DB_PASSWORD = "12345";
+    const DB_CHARSET = "charset=utf-8";
