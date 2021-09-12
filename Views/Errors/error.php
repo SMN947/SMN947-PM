b7ffa72ef21620105fba5689e578139bf236b2f8
SMN947 <betum98@gmail.com>
2021-09-12 03:01:49 -0500

    removed search from error page

--- Views/Errors/error.php
+++ Views/Errors/error.php
@@ -18,20 +18,8 @@
 
           <p>
             We could not find the page you were looking for.
-            Meanwhile, you may <a href="<?= BASE_URL ?>">return to dashboard</a> or try using the search form.
+            Meanwhile, you may <a href="<?= BASE_URL ?>">return to dashboard</a>.
           </p>
-
-          <form class="search-form">
-            <div class="input-group">
-              <input type="text" name="search" class="form-control" placeholder="Search">
-
-              <div class="input-group-append">
-                <button type="submit" name="submit" class="btn btn-warning"><i class="fas fa-search"></i>
-                </button>
-              </div>
-            </div>
-            <!-- /.input-group -->
-          </form>
         </div>
         <!-- /.error-content -->
       </div>
