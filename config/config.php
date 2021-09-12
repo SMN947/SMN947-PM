76e03d0aa1a1e5e8a0ccaff938042c80d355cc3a
SMN947 <betum98@gmail.com>
2021-09-12 02:44:54 -0500

    Updated config vars

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
     date_default_timezone_set("America/Bogota");
-    const BASE_URL = "http://localhost/SMN947-PM/";
+    const BASE_URL = "http://192.168.0.11/SMN947-PM";
     //Database
     const DB_HOST = "localhost";
     const DB_NAME = "smn947-pm";
@@ -8,5 +8,12 @@
     const DB_PASSWORD = "12345";
     const DB_CHARSET = "charset=utf-8";
     //Site Identity
-    const SITE_TITLE = "SMN947";
+    const SITE_TITLE = "SMN947-PM";
+    const SITE_DESC = "A project Management App";
+
+    //
+    const STYLES = 'Assets/css';
+    const SCRIPTS = 'Assets/js';
+    const IMAGES = 'Assets/images';
+    const FONTS = 'Assets/fonts';
 
