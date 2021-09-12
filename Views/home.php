0c392b57bc6d65dc344bb0bc1a7a665fdd49c02c
SMN947 <betum98@gmail.com>
2021-09-12 02:46:42 -0500

    Templated main dashboard View

--- Views/home.php
+++ Views/home.php
@@ -1,2 +1,20 @@
-<?= page_header($data); ?>
-<?= page_footer($data); ?>
+<?= load_template("header", $data); ?>
+<?= load_template("sidebar", $data); ?>
+<?= load_template("navbar", $data); ?>
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <br>
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        Little dashboard here :)
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  
+<?= load_template("footer", $data); ?>
