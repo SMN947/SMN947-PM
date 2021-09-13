4849eca3bbc8897a043a353004a2c5e1b3794bd1
SMN947 <betum98@gmail.com>
2021-09-13 17:32:18 -0500

    Aded functions to test SQL

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -5,8 +5,19 @@
         }
 
         public function home() {
-            $data["page_title"] = "Home";
+            $data["page_title"] = "Dasboard";
+            $data["page_path"] = "Home";
             $this->views->getView($this, "home", $data);
         } 
+
+        public function add() {
+            $data = $this->model->setUser("Simon", 5);
+            print_r($data);
+        }
+
+        public function get($id) {
+            $data = $this->model->getUser($id);
+            print_r($data);
+        }
     }
     
--- Models/homeModel.php
+++ Models/homeModel.php
@@ -1,7 +1,17 @@
 <?php
-    class homeModel {
+    class homeModel extends MySQL{
         public function __construct() {
-            echo "Se ha carrgado el mopdelo Home";
+            parent::__construct();
         }
-        
+        public function setUser(string $a, int $b) {
+            $query = "insert into test(matricula, cash) values (?, ?)";
+            $arr = array($a, $b);
+            $req = $this->insert($query, $arr);
+            return $req;
+        }
+        public function getUser(int $b) {
+            $query = "select * from test where id = $b";
+            $req = $this->select($query);
+            return $req;
+        }
     }
