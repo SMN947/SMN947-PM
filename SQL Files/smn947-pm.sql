25a1f1629d3a0e07cc79e8112100491df1b69fe7
SMN947 <betum98@gmail.com>
2021-09-15 16:40:15 -0500

    Updated sql model

--- SQL Files/smn947-pm.sql
+++ SQL Files/smn947-pm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-09-2021 a las 00:31:08
+-- Tiempo de generación: 15-09-2021 a las 23:39:32
 -- Versión del servidor: 10.4.20-MariaDB
 -- Versión de PHP: 7.4.22
 
@@ -24,6 +24,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `columns`
+--
+
+CREATE TABLE `columns` (
+  `column_id` int(11) NOT NULL,
+  `column_name` varchar(255) NOT NULL,
+  `column_description` varchar(4000) NOT NULL,
+  `column_position` int(11) NOT NULL,
+  `column_project_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `columns`
+--
+
+INSERT INTO `columns` (`column_id`, `column_name`, `column_description`, `column_position`, `column_project_id`) VALUES
+(1, 'To Do', '', 1, 22),
+(2, 'In Progress', '', 2, 22),
+(3, 'For Review', '', 3, 22),
+(4, 'Done', '', 4, 22),
+(5, 'To Do', '', 1, 23),
+(6, 'In Progress', '', 2, 23),
+(7, 'For Review', '', 3, 23),
+(8, 'Done', '', 4, 23);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `projects`
 --
 
@@ -32,8 +60,16 @@ CREATE TABLE `projects` (
   `project_name` varchar(45) NOT NULL,
   `project_description` varchar(4000) CHARACTER SET utf8 DEFAULT NULL,
   `project_user_creator` int(11) DEFAULT NULL,
-  `project_ceation_date` datetime DEFAULT NULL
+  `project_ceation_date` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `projects`
+--
+
+INSERT INTO `projects` (`project_id`, `project_name`, `project_description`, `project_user_creator`, `project_ceation_date`) VALUES
+(22, 'TestProject', 'La descripcion del project :) ?', 1, '2021-09-15 14:02:48'),
+(23, 'poiuytr', 'poiu', 1, '2021-09-15 15:30:02');
 
 -- --------------------------------------------------------
 
@@ -49,8 +85,22 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_password`) VALUES
+(1, 'SMN947', 'smn947@smn947.com', '12345');
+
+--
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `columns`
+--
+ALTER TABLE `columns`
+  ADD PRIMARY KEY (`column_id`),
+  ADD KEY `project_id` (`column_project_id`);
 
 --
 -- Indices de la tabla `projects`
@@ -70,20 +120,32 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `columns`
+--
+ALTER TABLE `columns`
+  MODIFY `column_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT de la tabla `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `columns`
+--
+ALTER TABLE `columns`
+  ADD CONSTRAINT `project_id` FOREIGN KEY (`column_project_id`) REFERENCES `projects` (`project_id`);
 
 --
 -- Filtros para la tabla `projects`
