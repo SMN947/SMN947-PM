943fd9617ddc9d1bf2748f1e428e817001ddf455
SMN947 <betum98@gmail.com>
2021-09-17 17:58:47 -0500

    Templated the add Task view

---
+++ Views/Project/add_task.php
@@ -0,0 +1,24 @@
+<!-- /.login-logo -->
+<div class="card card-outline card-primary">
+  <div class="card-body">
+    <p class="login-box-msg">Sign in to start your session</p>
+
+    <form action="<?= BASE_URL ?>/project/create_save/" method="get">
+      <div class="input-group mb-3">
+        <input name="project-name" type="text" class="form-control" placeholder="Project Name">
+      </div>
+      <div class="input-group mb-3">
+        <input name="project-desc" type="text" class="form-control" placeholder="Project Description">
+      </div>
+      <div class="row">
+        <!-- /.col -->
+        <div class="col-4">
+          <button type="submit" class="btn btn-primary btn-block">Create</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
+  </div>
+  <!-- /.card-body -->
+</div>
+<!-- /.card -->
