2cf3262e4218115df2ab2d60a254e77c56893745
SMN947 <betum98@gmail.com>
2021-09-26 22:51:55 -0500

    Implemented drag&drop between columns

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -14,3 +14,45 @@
     $('#myModal').modal('show')
     $("#exampleModalLabel").html(modalTitle)
 });
+
+$(function () {
+    $(".sortable")
+      .sortable({
+        revert: true,
+        placeholder: "drag-place-holder",
+        forcePlaceholderSize: true,
+        connectWith: ".sortable",
+        helper: function (event, element) {
+          return $(element).clone().addClass("dragging");
+        },
+        start: function (e, ui) {
+          ui.item.show().addClass("ghost");
+        },
+        stop: function (e, ui) {
+          ui.item.show().removeClass("ghost");
+        },
+        receive: function (e, ui) {
+            var newCol = e.target.id;
+            var taskId = ui.item[0].id;
+            var taskData = JSON.parse($("#task_"+taskId).val())
+            taskData.task_column_id = newCol;
+            console.log(`Task ${taskId} now in column ${newCol}`)
+            var URLUpdate = `${BASE_URL}/project/edit_task/${taskId}?data=${JSON.stringify(taskData)}`
+            
+            $.ajax(URLUpdate)
+                .done(function(a) {
+                    console.log(a)
+                    console.log( "success" );
+                })
+                .fail(function() {
+                    console.log( "error" );
+                })
+                .always(function() {
+                    console.log( "complete" );
+                });
+                console.log(BASE_URL)
+        },
+        cursor: "move"
+      })
+      .disableSelection();
+  });
