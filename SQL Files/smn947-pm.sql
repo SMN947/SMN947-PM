5ebea4145d24de112a81399c71aa132df8683f92
SMN947 <betum98@gmail.com>
2021-09-26 22:57:33 -0500

    Latest SQL Template

--- SQL Files/smn947-pm.sql
+++ SQL Files/smn947-pm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-09-2021 a las 23:39:32
+-- Tiempo de generación: 27-09-2021 a las 05:56:45
 -- Versión del servidor: 10.4.20-MariaDB
 -- Versión de PHP: 7.4.22
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `smn947-pm`
 --
+CREATE DATABASE IF NOT EXISTS `smn947-pm` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `smn947-pm`;
 
 -- --------------------------------------------------------
 
@@ -47,7 +49,11 @@ INSERT INTO `columns` (`column_id`, `column_name`, `column_description`, `column
 (5, 'To Do', '', 1, 23),
 (6, 'In Progress', '', 2, 23),
 (7, 'For Review', '', 3, 23),
-(8, 'Done', '', 4, 23);
+(8, 'Done', '', 4, 23),
+(9, 'To Do', '', 5, 24),
+(10, 'In Progress', '', 2, 24),
+(11, 'For Review', '', 3, 24),
+(12, 'Done', '', 4, 24);
 
 -- --------------------------------------------------------
 
@@ -69,7 +75,37 @@ CREATE TABLE `projects` (
 
 INSERT INTO `projects` (`project_id`, `project_name`, `project_description`, `project_user_creator`, `project_ceation_date`) VALUES
 (22, 'TestProject', 'La descripcion del project :) ?', 1, '2021-09-15 14:02:48'),
-(23, 'poiuytr', 'poiu', 1, '2021-09-15 15:30:02');
+(23, 'poiuytr', 'poiu', 1, '2021-09-15 15:30:02'),
+(24, 'dds', 'dsss', 1, '2021-09-17 16:54:24');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `task_id` int(11) NOT NULL,
+  `task_name` varchar(255) NOT NULL,
+  `task_desc` varchar(4000) NOT NULL,
+  `task_start_sch` date NOT NULL,
+  `task_end_sch` date NOT NULL,
+  `task_project_id` int(11) NOT NULL,
+  `task_column_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tasks`
+--
+
+INSERT INTO `tasks` (`task_id`, `task_name`, `task_desc`, `task_start_sch`, `task_end_sch`, `task_project_id`, `task_column_id`) VALUES
+(7, 'Test Task', 'DEscription Task', '2021-08-30', '2021-09-30', 22, 2),
+(8, 'Test Task 23 1', 'DEscription Task', '2021-08-30', '2021-09-22', 22, 2),
+(9, 'Test Task', 'cdsdsvdsv', '2021-08-29', '2021-09-28', 22, 1),
+(10, 'zzx zx ', 'zxvdsdh s dj dj dd  ', '2021-09-07', '2021-09-24', 22, 1),
+(11, 'shs sh sh sh shs h', ' hrkx  c,d kdk x kdkxs s t dt dtkdtkdtk', '2021-09-07', '2021-09-24', 22, 1),
+(12, 'xbnh', 'shshsfhfsh', '2021-09-01', '2021-09-24', 24, 10),
+(13, 'jenni', 'loviu <3', '2021-09-08', '2021-10-02', 22, 1);
 
 -- --------------------------------------------------------
 
@@ -110,6 +146,12 @@ ALTER TABLE `projects`
   ADD KEY `user_id` (`project_user_creator`);
 
 --
+-- Indices de la tabla `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`task_id`);
+
+--
 -- Indices de la tabla `users`
 --
 ALTER TABLE `users`
@@ -123,13 +165,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `columns`
 --
 ALTER TABLE `columns`
-  MODIFY `column_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `column_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT de la tabla `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
